a68eb307f9bed689f862ed30466e6a37e57afeb4
julian velez gaitan <juliansolo7189@gmail.com>
2023-07-23 19:53:05 -0500

    splash screen

---
+++ main.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", function() {
+	// Cuando la página ha cargado, mostramos el contenido principal
+	document.querySelector(".main-content").classList.add("show");
+	// Y ocultamos el splash screen
+	document.querySelector(".splash-screen").classList.add("hide");
+});
